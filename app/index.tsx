270fbdc43199e4a6ff21a3442a726c7fd4567b6b
xd <guilhermeoliveira@preve.g12.br>
2026-04-10 14:51:00 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>🤓</Text>
+      <Link href="/about" style={styles.button}>Vai para o Sobre</Link>
+    </View>
+  );
+      }
+    
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#000000',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      text: {
+        color: '#ff0000',
+      },
+      button: {
+        fontSize: 20,
+        textDecorationLine: 'underline',
+        color: '#8c05fa',
+      }
+    });
